dbd7acbafd5f358bdec14b357c21a841dec1f9c8
meraj012 <meraj84091@gmail.com>
2023-08-30 14:23:26 +0530

    check-prime

---
+++ prime.js
@@ -0,0 +1,13 @@
+
+let flag= true;
+for(i = 2; i <= n - 1; i++){
+    if (n % i == 0) {
+      flag = false;
+      break;
+    }
+  }               
+     // Check and display alert message
+   if (flag == true)
+    alert(n + " is prime");
+   else
+    alert(n + " is not prime");
